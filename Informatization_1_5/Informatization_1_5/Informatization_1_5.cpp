bd7f5ca4aee4e958a51a00da62fd8373c95bba57
Hemul997 <nikitamoshkin1997@gmail.com>
2017-02-12 12:20:41 +0300

    Добавлен класс для постройки

--- Informatization_1_5/Informatization_1_5/Informatization_1_5.cpp
+++ Informatization_1_5/Informatization_1_5/Informatization_1_5.cpp
@@ -1,7 +1,7 @@
 ﻿// Informatization_1_5.cpp : Defines the entry point for the console application.
 //Mошкин Никита ПС-22.
 //1.5. Информатизация садоводства (8)
-//Дачный участок Степана Петровича имеет форму прямоугольника размером a  b.На участке
+//Дачный участок Степана Петровича имеет форму прямоугольника размером a x b.На участке
 //имеется n построек, причем основание каждой постройки — прямоугольник со сторонами,
 //параллельными сторонам участка.
 //Вдохновленный успехами соседей, Степан Петрович хочет посадить на своем участке m видов
@@ -15,10 +15,68 @@
 //построек определяет оптимальное расположение планируемых грядок.
 
 #include "stdafx.h"
+#include <fstream>
 
+using namespace std;
 
+struct Point
+{
+	int x, y;
+};
+
+class Building
+{
+	
+public:
+	Building(int x1, int y1, int x2, int y2)
+	{
+		A.x = x1;
+		A.y = y1;
+		C.x = x2;
+		C.y = y2;
+		FindBuildingArea(A, C);
+		
+	}
+	
+	void FindBuildingArea(Point A, Point C)
+	{
+		BuildingArea = (C.x - A.x)*(C.y - A.y);
+	}
+	int GetBuildingArea()const
+	{
+		return BuildingArea;
+	}
+private:
+	Point A;
+	Point C;
+	int BuildingArea;
+};
+bool ErrorOfCoordinats(int x1, int y1, int x2, int y2)
+{
+	return(x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0);
+}
+bool ReadStream(fstream input, int x1, int y1 , int x2 ,int y2)
+{
+	input >> x1;
+	input >> y1;
+	input >> x2;
+	input >> y2;
+	return (ErrorOfCoordinats(x1, y1, x2, y2));
+}
+bool ErrorsOfSize(int a, int b)
+{
+	return(a < 1 || b < 1 || b > 10000);
+}
+bool ErrorsOfCounts(int m, int n)
+{
+	return(n < 0 || n > 10 || m > 2 || m < 1);
+}
 int main()
 {
+	int x1, y1, x2, y2, n, m, a, b;
+
+	fstream input("input.txt");
+	fstream output("output.txt");
     return 0;
 }
 
