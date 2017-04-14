f0c90407132e512147f25751fe2fb304465eb101
Hemul997 <nikitamoshkin1997@gmail.com>
2017-04-14 10:26:53 +0300

    Изменил хранение координат вирусов в списке а не в векторе. Добавил вывод отсортированного списка

--- Viruces/Viruces/Viruces.cpp
+++ Viruces/Viruces/Viruces.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <list>
 const int MAX_SIZE = 50;
 const int MIN_SIZE = 2;
 const int MAX_COUNT_VIRUCES = 10;
@@ -17,10 +18,11 @@ struct Virus {
 };
 int main()
 {
+	int field[MAX_SIZE][MAX_SIZE];
 	int sizeOfField;
 	int countOfViruces;
 	ifstream inputFile("input.txt");
-	vector<Virus> viruses;
+	list<Virus> viruses;
 	inputFile >> sizeOfField >> countOfViruces;
 	if (sizeOfField < MIN_SIZE || sizeOfField > MAX_SIZE || countOfViruces > MAX_COUNT_VIRUCES || countOfViruces < MIN_COUNT_VIRUCES)
 	{
@@ -33,7 +35,19 @@ int main()
 		inputFile >> virus.x >> virus.y;
 		viruses.push_back(virus);
 	}
-
+	viruses.sort();
+	cout << "Sorted list of coordinats viruses\n";
+	for (auto it = viruses.begin(); it != viruses.end(); ++it)
+	{
+		cout << (*it).x << (*it).y << endl;
+	}
+	/*for (auto i = 0; i < sizeOfField; ++i)
+	{
+		for (auto j = 0; j < sizeOfField; ++j)
+		{
+			if
+		}
+	}*/
     return 0;
 }
 
