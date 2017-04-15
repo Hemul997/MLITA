e472924b195d0eae76ce801770874251da82e43e
Hemul997 <nikitamoshkin1997@gmail.com>
2017-04-16 01:32:37 +0300

    Добавил функции по высчитыванию кольцевого хэша

--- Rabin-Carp/Rabin-Carp/Rabin-Carp.cpp
+++ Rabin-Carp/Rabin-Carp/Rabin-Carp.cpp
@@ -10,39 +10,47 @@
 
 using namespace std;
 
+const int NUMERATOR = 52;
+const int DENOMENATOR = 65713;
+
+int numberInitialization(size_t searchStrSize)//проверка на входной хэш
+{
+	int number = 1;
+	for (auto i = 0; i < searchStrSize; ++i)
+	{
+		number = (number*NUMERATOR) % DENOMENATOR;
+	}
+	return number;
+}
+int ringHash(string searchString, size_t searchStrSize, int prevHash, int number)
+{
+	if (number == 0)
+	{
+		number = numberInitialization(searchStrSize);
+	}
+	if (prevHash == 0)
+	{
+		for (auto i = 0; i < searchStrSize; ++i)
+		{
+			prevHash = (prevHash*NUMERATOR + (int)searchString[i]) % DENOMENATOR;
+		}
+		return prevHash;
+	}
+	else
+	{
+		int hash;
+		if (hash < 0) 
+		{
+			hash += DENOMENATOR;
+		}
+		return hash;
+	}
+}
 int main()
 {
 	string searchString, inputText; // входные данные
 
-				 // считаем все степени p
-	const int p = 31;
-	vector<long long> p_pow(max(searchString.length(), inputText.length()));
-	p_pow[0] = 1;
-	for (size_t i = 1; i<p_pow.size(); ++i)
-		p_pow[i] = p_pow[i - 1] * p;
-
-	// считаем хэши от всех префиксов строки T
-	vector<long long> h(inputText.length());
-	for (size_t i = 0; i<inputText.length(); ++i)
-	{
-		h[i] = (inputText[i] - 'a' + 1) * p_pow[i];
-		if (i)  h[i] += h[i - 1];
-	}
-
-	// считаем хэш от строки S
-	long long h_s = 0;
-	for (size_t i = 0; i<searchString.length(); ++i)
-		h_s += (searchString[i] - 'a' + 1) * p_pow[i];
-
-	// перебираем все подстроки T длины |S| и сравниваем их
-	for (size_t i = 0; i + searchString.length() - 1 < inputText.length(); ++i)
-	{
-		long long cur_h = h[i + searchString.length() - 1];
-		if (i)  cur_h -= h[i - 1];
-		// приводим хэши к одной степени и сравниваем
-		if (cur_h == h_s * p_pow[i])
-			cout << i << ' ';
-	}
+	
     return 0;
 }
 
--- Viruces/Viruces/Viruces.cpp
+++ Viruces/Viruces/Viruces.cpp
@@ -6,23 +6,28 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <queue>
+
 const int MAX_SIZE = 50;
 const int MIN_SIZE = 2;
 const int MAX_COUNT_VIRUCES = 10;
 const int MIN_COUNT_VIRUCES = 1;
 
+typedef int field[MAX_SIZE][MAX_SIZE];
 using namespace std;
 
 struct Virus {
 	int x, y;
 };
+void pushTheViruses(int sizeOfField, field workField, int x, int y);
 int main()
 {
-	int field[MAX_SIZE][MAX_SIZE];
+	field workField;
 	int sizeOfField;
 	int countOfViruces;
 	ifstream inputFile("input.txt");
 	list<Virus> viruses;
+	int top = 0;
 	inputFile >> sizeOfField >> countOfViruces;
 	if (sizeOfField < MIN_SIZE || sizeOfField > MAX_SIZE || countOfViruces > MAX_COUNT_VIRUCES || countOfViruces < MIN_COUNT_VIRUCES)
 	{
@@ -35,19 +40,49 @@ int main()
 		inputFile >> virus.x >> virus.y;
 		viruses.push_back(virus);
 	}
-	viruses.sort();
-	cout << "Sorted list of coordinats viruses\n";
 	for (auto it = viruses.begin(); it != viruses.end(); ++it)
 	{
-		cout << (*it).x << (*it).y << endl;
+		pushTheViruses(sizeOfField, workField, (*it).x, (*it).y);
 	}
-	/*for (auto i = 0; i < sizeOfField; ++i)
+	for (auto i = 1; i <= sizeOfField; ++i)
 	{
-		for (auto j = 0; j < sizeOfField; ++j)
+		for (auto j = 1; j <= sizeOfField; ++j)
 		{
-			if
+			cout << workField[i][j] << ' ';
 		}
-	}*/
+		cout << endl;
+	}
+	for (auto i = 1; i <= sizeOfField; ++i)
+	{
+		for (auto j = 1; j <= sizeOfField; ++j)
+		{
+			if (workField[i][j] == 0)
+			{
+
+			}
+		}
+	}
+	cout << top << endl;
     return 0;
 }
 
+void pushTheViruses(int sizeOfField, field workField, int x, int y)
+{
+	for (auto i = 1; i <= sizeOfField; ++i)
+	{
+		for (auto j = 1; j <= sizeOfField; ++j)
+		{
+			if (i == x && j == y)
+			{
+				workField[i][j] = 0;
+			}
+			else
+			{
+				if (workField[i][j] != 0)
+				{
+					workField[i][j] = -1;
+				}
+			}
+		}
+	}
+}
